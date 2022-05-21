be2a367147c5f8583d717f0402002a9dd3f74428
Albina <axioma.25@gmail.com>
2022-05-21 15:56:14 +0500

    4 task done

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,23 @@
+const fs = require('fs');
+const fsPromises = fs.promises;
+const path = require('path');
 
+const srcDir = path.join(__dirname, 'files');
+const targetDir = path.join(__dirname, 'files-copy');
+fsPromises.mkdir(targetDir, { recursive: true }, (err) => {
+  if (err) throw err;
+});
+
+function callback(err) {
+  if (err) throw err;
+}
+
+fs.readdir(srcDir, (err, files) => {
+  if (err)
+    console.log(err);
+  else {
+    files.forEach(file => {
+      fs.copyFile(path.join(srcDir, file), path.join(targetDir, file), callback);
+    })
+  }
+})
